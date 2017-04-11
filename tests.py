70006f21d565545a6865f9517587279e1fdf90bb
Thomas Ward <teward@ubuntu.com>
2017-04-11 15:41:02 -0400

    More verbose test case - however, this requires a 'test' IMAP server...

--- tests.py
+++ tests.py
@@ -1,1 +1,27 @@
 import imaplibext
+import unittest
+
+
+class TestIMAPMethods(unittest.TestCase):
+
+    def test_imap4_plain(self):
+        # type: () -> None
+        # noinspection PyUnusedLocal
+        conn = None
+        conn = imaplibext.IMAP4('mail.dark-net.io', 143)
+        self.assertIsNotNone(conn, 'Expected an actual conn here.')
+        self.assertEqual(conn.state, 'NONAUTH', "Expected non-authenticated connection state.")
+        return None  # Python 2 Type Hinting Compliance
+
+    def test_imap4_ssl(self):
+        # type: () -> None
+        # noinspection PyUnusedLocal
+        conn = None
+        conn = imaplibext.IMAP4_SSL('mail.dark-net.io', 993)
+        self.assertIsNotNone(conn, 'Expected an actual conn here.')
+        self.assertEqual(conn.state, 'NONAUTH', "Expected non-authenticated connection state.")
+        return None  # Python 2 Type Hinting Compliance
+
+
+if __name__ == '__main__':
+    unittest.main()
