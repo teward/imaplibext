47d9065445bd2baafcc1173e7e689bb708314af5
Thomas Ward <teward@ubuntu.com>
2017-06-19 12:23:38 -0400

    bump version

--- imaplibext/__init__.py
+++ imaplibext/__init__.py
@@ -5,7 +5,7 @@ from ._imaplibext import (
 
 __title__ = 'imaplibext'
 __author__ = 'Thomas Ward'
-__version__ = '0.4.4'
+__version__ = '0.4.5'
 __copyright__ = '2017 Thomas Ward'
 __license__ = 'AGPLv3+'
 __all__ = ('IMAP4', 'IMAP4_SSL')
