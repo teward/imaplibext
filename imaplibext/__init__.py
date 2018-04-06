1e30316199c2a297d2216512a17db23855622045
Thomas Ward <teward@ubuntu.com>
2018-04-06 11:28:06 -0400

    REVERT 0.7.1 changes.

--- imaplibext/__init__.py
+++ imaplibext/__init__.py
@@ -5,7 +5,7 @@ from ._imaplibext import (
 
 __title__ = 'imaplibext'
 __author__ = 'Thomas Ward'
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 __copyright__ = '2018 Thomas Ward'
 __license__ = 'AGPLv3+'
 __all__ = ('IMAP4', 'IMAP4_SSL')
