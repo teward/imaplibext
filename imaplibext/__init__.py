6734b4d4a8e5e45b5db1cc21eb61632ef2c05be4
Thomas Ward <teward@thomas-ward.net>
2019-01-29 20:53:32 -0500

    Copyright update: It's now 2019. Also update format properly.

--- imaplibext/__init__.py
+++ imaplibext/__init__.py
@@ -6,6 +6,6 @@ from ._imaplibext import (
 __title__ = 'imaplibext'
 __author__ = 'Thomas Ward'
 __version__ = '0.7.6'
-__copyright__ = '2018 Thomas Ward'
+__copyright__ = 'Copyright 2018-2019, Thomas Ward'
 __license__ = 'AGPLv3+'
 __all__ = ('IMAP4', 'IMAP4_SSL')
